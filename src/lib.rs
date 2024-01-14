8b1728f8b7b709f5b46ec78b143f07ff23616173
Junho Yeo <i@junho.io>
2024-01-15 02:16:05 +0900

    Add `#[cfg(test)]` to `integration_tests`

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ pub mod contract;
 pub mod error;
 pub mod execute;
 pub mod helpers;
-pub mod integration_tests;
 pub mod msg;
 pub mod query;
 pub mod states;
 
-pub use crate::error::ContractError;
+#[cfg(test)]
+pub mod integration_tests;
