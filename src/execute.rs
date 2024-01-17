77cd45f522bf2a2470a9d3d3fdda2427c841190b
Junho Yeo <i@junho.io>
2024-01-18 00:07:52 +0900

    Refactor: use name `swapped_in` inside `swap`

--- src/execute.rs
+++ src/execute.rs
@@ -73,14 +73,14 @@ pub fn swap(deps: DepsMut, _env: Env, info: MessageInfo) -> Result<SwapResult, C
 
     let input_token_denom = "uusd";
 
-    // swapped_in
-    let burned_uusd = info
+    // burned_uusd
+    let swapped_in = info
         .funds
         .iter()
         .find(|c| c.denom == input_token_denom)
         .map(|c| c.amount)
         .unwrap_or_else(Uint128::zero);
-    if burned_uusd.is_zero() {
+    if swapped_in.is_zero() {
         return Err(ContractError::NotAllowZeroAmount {});
     }
     if info.funds.len() > 1 {
@@ -98,30 +98,30 @@ pub fn swap(deps: DepsMut, _env: Env, info: MessageInfo) -> Result<SwapResult, C
 
     let k = state.x_liquidity * state.y_liquidity;
 
-    if state.y_liquidity + burned_uusd == Uint128::zero() {
+    if state.y_liquidity + swapped_in == Uint128::zero() {
         return Err(ContractError::DivisionByZeroError {});
     }
 
-    let swapped_out = state.x_liquidity - (k / (state.y_liquidity + burned_uusd));
+    let swapped_out = state.x_liquidity - (k / (state.y_liquidity + swapped_in));
     if state.total_swapped + swapped_out > config.sale_amount {
         return Err(ContractError::PoolSizeExceeded {
             available: config.sale_amount - state.total_swapped,
         });
     }
 
-    let virtual_slippage = (swapped_out * price) / burned_uusd;
-    user.burned_uusd += burned_uusd;
+    let virtual_slippage = (swapped_out * price) / swapped_in;
+    user.burned_uusd += swapped_in;
     user.swapped_out += swapped_out - virtual_slippage;
 
     state.total_swapped += swapped_out;
-    state.x_liquidity += burned_uusd;
+    state.x_liquidity += swapped_in;
     state.y_liquidity -= swapped_out;
 
     USER.save(deps.storage, info.sender.as_bytes(), &user)?;
     STATE.save(deps.storage, &state)?;
 
     let deposit_result = SwapResult {
-        swapped_in: burned_uusd,
+        swapped_in,
         swapped_out,
     };
     Ok(deposit_result)
