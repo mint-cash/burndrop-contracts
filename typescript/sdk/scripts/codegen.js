32a00975619c1b8df6fb0efc1d772a64530c976c
Junho Yeo <i@junho.io>
2024-01-18 16:29:08 +0900

    Format generated code with Prettier

--- typescript/sdk/scripts/codegen.js
+++ typescript/sdk/scripts/codegen.js
@@ -2,6 +2,7 @@ const codegen = require('@cosmwasm/ts-codegen').default;
 const path = require('path');
 const fs = require('fs');
 const findWorkspaceRoot = require('find-yarn-workspace-root');
+const prettier = require('prettier');
 
 const YARN_WORKSPACE_ROOT = findWorkspaceRoot();
 const schemaDir = path.join(YARN_WORKSPACE_ROOT, 'schema');
@@ -9,29 +10,56 @@ const schemaDir = path.join(YARN_WORKSPACE_ROOT, 'schema');
 const outPath = path.join(YARN_WORKSPACE_ROOT, 'typescript', 'sdk', 'src');
 fs.rmSync(outPath, { recursive: true, force: true });
 
-codegen({
-  contracts: [{ name: 'burndrop', dir: schemaDir }],
-  outPath,
-  options: {
-    bundle: {
-      bundleFile: 'index.ts',
-      scope: 'contracts',
+const PRETTIER_CONFIG_PATH = path.join(YARN_WORKSPACE_ROOT, '.prettierrc');
+
+const main = async () => {
+  // Codegen
+  await codegen({
+    contracts: [{ name: 'burndrop', dir: schemaDir }],
+    outPath,
+    options: {
+      bundle: {
+        bundleFile: 'index.ts',
+        scope: 'contracts',
+      },
+      client: {
+        enabled: true,
+      },
+      reactQuery: {
+        enabled: true,
+        version: 'v5',
+        mutations: false,
+      },
+      recoil: {
+        enabled: true,
+      },
+      messageComposer: {
+        enabled: true,
+      },
     },
-    client: {
-      enabled: true,
-    },
-    reactQuery: {
-      enabled: true,
-      version: 'v5',
-      mutations: false,
-    },
-    recoil: {
-      enabled: true,
-    },
-    messageComposer: {
-      enabled: true,
-    },
-  },
-}).then(() => {
+  });
   console.log('✨ Typescript code is generated successfully!');
-});
+
+  // Prettier
+  const config = await prettier.resolveConfig(PRETTIER_CONFIG_PATH);
+  const files = fs.readdirSync(outPath);
+  const promises = await Promise.all(
+    files.map(async (file) => {
+      const filePath = path.join(outPath, file);
+      const formatted = await prettier.format(
+        fs.readFileSync(filePath, 'utf8'),
+        {
+          parser: 'typescript',
+          ...config,
+        },
+      );
+      return [filePath, formatted];
+    }),
+  );
+  promises.forEach(([filePath, formatted]) => {
+    fs.writeFileSync(filePath, formatted);
+  });
+  console.log('💅 Typescript code is formatted successfully!');
+};
+
+main();
