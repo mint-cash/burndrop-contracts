751cbb10a27b8800a73fcf6a887eeed996e8c989
Huiseong Yang <devonnuri@gmail.com>
2024-01-23 20:56:44 +0900

    Write tests about updated swap period

--- src/integration_tests.rs
+++ src/integration_tests.rs
@@ -117,10 +117,9 @@ mod tests {
         use super::*;
 
         #[test]
-        fn test_burn_tokens() {
+        fn test_burn_tokens_during_period() {
             let (mut app, burn_contract) = proper_instantiate();
             // Try to burn some tokens for a user with a referrer.
-
             let burn_amount = Uint128::new(100);
             let sender_info = mock_info(
                 USER,
@@ -198,7 +197,113 @@ mod tests {
             );
             assert!(execute_res.is_err());
         }
-        // Add more tests for other functionalities like error cases.
+
+        #[test]
+        fn test_burn_tokens_not_modified_period() {
+            let (mut app, burn_contract) = proper_instantiate();
+
+            // Modify the swap period to be shorter.
+            let modify_msg = ExecuteMsg::UpdateRounds {
+                rounds: vec![SwapRound {
+                    start_time: 1706001400,
+                    end_time: 1706001500, // modify end_time
+                    output_token: OutputToken::OppaMINT,
+                }],
+            };
+            let cosmos_msg = burn_contract.call(modify_msg).unwrap();
+            let modify_res = app.execute(Addr::unchecked(ADMIN), cosmos_msg);
+            assert!(modify_res.is_ok());
+
+            // Try to burn some tokens for a user with a referrer.
+            let burn_amount = Uint128::new(100);
+            let sender_info = mock_info(
+                USER,
+                &[Coin {
+                    denom: NATIVE_DENOM.to_string(),
+                    amount: burn_amount,
+                }],
+            );
+
+            // Create a burn tokens message
+            let burn_msg = ExecuteMsg::BurnTokens {
+                amount: burn_amount,
+                referrer: REFERRER.to_string(),
+            };
+            app.update_block(|block| {
+                block.time = Timestamp::from_seconds(1706001506); // in the previous swap period, not in the modified period
+            });
+            let burn_res = app.execute_contract(
+                Addr::unchecked(USER),
+                burn_contract.addr(),
+                &burn_msg,
+                &sender_info.funds,
+            );
+            assert!(burn_res.is_err());
+        }
+
+        #[test]
+        fn test_burn_tokens_during_modified_period() {
+            let (mut app, burn_contract) = proper_instantiate();
+
+            // Modify the swap period to be shorter.
+            let modify_msg = ExecuteMsg::UpdateRounds {
+                rounds: vec![SwapRound {
+                    start_time: 1706001400,
+                    end_time: 1706001700, // modify end_time
+                    output_token: OutputToken::OppaMINT,
+                }],
+            };
+            let cosmos_msg = burn_contract.call(modify_msg).unwrap();
+            let modify_res = app.execute(Addr::unchecked(ADMIN), cosmos_msg);
+            assert!(modify_res.is_ok());
+
+            // Try to burn some tokens for a user with a referrer.
+            let burn_amount = Uint128::new(100);
+            let sender_info = mock_info(
+                USER,
+                &[Coin {
+                    denom: NATIVE_DENOM.to_string(),
+                    amount: burn_amount,
+                }],
+            );
+
+            // Create a burn tokens message
+            let burn_msg = ExecuteMsg::BurnTokens {
+                amount: burn_amount,
+                referrer: REFERRER.to_string(),
+            };
+            app.update_block(|block| {
+                block.time = Timestamp::from_seconds(1706001680); // in the modified swap period
+            });
+            let burn_res = app.execute_contract(
+                Addr::unchecked(USER),
+                burn_contract.addr(),
+                &burn_msg,
+                &sender_info.funds,
+            );
+            assert!(burn_res.is_ok());
+
+            // Query the burn info after burning tokens for the user.
+            let query_res: crate::msg::UserInfoResponse = app
+                .wrap()
+                .query_wasm_smart(
+                    burn_contract.addr(),
+                    &QueryMsg::UserInfo {
+                        address: USER.to_string(),
+                    },
+                )
+                .unwrap();
+
+            // Perform assertions based on the expected state after burning tokens.
+            assert_eq!(query_res.slot_size, Uint128::new(1000));
+            assert_eq!(query_res.slots, Uint128::new(1));
+            assert_eq!(query_res.cap, Uint128::new(1000));
+
+            assert_eq!(query_res.burned, Uint128::new(100));
+            assert_eq!(query_res.burnable, Uint128::new(900));
+            assert_eq!(query_res.swapped_out.oppamint, Uint128::new(196)); // 200 - virtual_slippage (4)
+            assert_eq!(query_res.swapped_out.ancs, Uint128::new(0));
+        }
     }
 
     mod query_tests {
