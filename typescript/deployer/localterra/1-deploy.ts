1bf717f602eb681db43846c4419ad6a3d0a7f95f
Junho Yeo <i@junho.io>
2024-01-30 22:48:28 +0900

    Implement contract deployer to local testnet
    wip
    
    wip
    
    deploy works in localnet
    
    simulate
    
    Estimate instantiate gas
    
    Must use cosmwasm/rust-optimizer:0.12.11
    
    fixuo

---
+++ typescript/deployer/localterra/1-deploy.ts
@@ -0,0 +1,177 @@
+import { OfflineSigner } from '@cosmjs/proto-signing';
+import { InstantiateMsg } from '@mint-cash/burndrop-sdk/dist/Burndrop.types';
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate';
+import { GasPrice, calculateFee } from '@cosmjs/stargate';
+import math from '@cosmjs/math';
+import long from 'long';
+import fs from 'fs';
+import amino from '@cosmjs/amino';
+import findWorkspaceRoot from 'find-yarn-workspace-root';
+import path from 'path';
+import { Secp256k1HdWallet, Secp256k1Wallet } from '@cosmjs/amino';
+import { stringToPath } from '@cosmjs/crypto';
+import encoding from '@cosmjs/encoding';
+import tx_4 from 'cosmjs-types/cosmwasm/wasm/v1/tx';
+
+const chainId = 'localterra' as const;
+type ConfigArgs = {
+  mnemonic?: string;
+  privateKey?: string;
+  endpoint: string;
+};
+
+class Config {
+  prefix = 'terra';
+
+  constructor(public args: ConfigArgs) {}
+
+  async getSigner(): Promise<OfflineSigner> {
+    let signer: Secp256k1HdWallet | Secp256k1Wallet | undefined = undefined;
+
+    if (this.args.mnemonic) {
+      signer = await Secp256k1HdWallet.fromMnemonic(this.args.mnemonic, {
+        prefix: this.prefix,
+        hdPaths: [stringToPath("m/44'/330'/0'/0/0")],
+      });
+    } else if (this.args.privateKey) {
+      signer = await Secp256k1Wallet.fromKey(
+        Buffer.from(this.args.privateKey, 'hex'),
+        this.prefix,
+      );
+    }
+
+    if (!signer) {
+      throw Error('no mnemonic or privkey');
+    }
+
+    return signer;
+  }
+
+  get command(): string {
+    return `terrad --node ${this.args.endpoint} --chain-id ${chainId}`;
+  }
+}
+
+const config = new Config({
+  endpoint: 'http://localhost:26657',
+  mnemonic:
+    'maid grocery wire attend bench physical merge case fringe drink group symbol embrace hurt such cost spray soon bullet produce minute lawsuit slab exhaust',
+  // 'notice oak worry limit wrap speak medal online prefer cluster roof addict wrist behave treat actual wasp year salad speed social layer crew genius',
+});
+
+const YARN_WORKSPACE_ROOT = findWorkspaceRoot();
+const WASM_PATH = path.join(
+  YARN_WORKSPACE_ROOT!,
+  'artifacts',
+  'burndrop_contracts.wasm',
+);
+
+async function main() {
+  const signer = await config.getSigner();
+  const [{ address: sender }] = await signer.getAccounts();
+
+  const client = await SigningCosmWasmClient.connectWithSigner(
+    config.args.endpoint,
+    signer,
+    { gasPrice: GasPrice.fromString('0.02uluna') },
+  );
+
+  const block = await client.getBlock();
+  console.log(block);
+
+  console.log(WASM_PATH, sender);
+  const wasm = fs.readFileSync(WASM_PATH);
+
+  const result = await client.upload(sender, wasm, 'auto');
+  console.log(result);
+
+  const instantiateMsg: InstantiateMsg = {
+    initial_slot_size: '1000',
+
+    rounds: [
+      {
+        id: 1,
+
+        start_time: 1706001400,
+        end_time: 1706001650,
+
+        oppamint_liquidity: {
+          x: '1000000',
+          y: '500000',
+        },
+        ancs_liquidity: {
+          x: '1000000',
+          y: '500000',
+        },
+      },
+    ],
+
+    max_query_limit: 30,
+    default_query_limit: 10,
+  };
+
+  // const gas = await client.simulate(sender, messages, undefined)
+
+  //   const instantiateContractMsg = {
+  //     typeUrl: "/cosmwasm.wasm.v1.MsgInstantiateContract",
+  //     value: tx_4.MsgInstantiateContract.fromPartial({
+  //         sender: senderAddress,
+  //         codeId: long_1.default.fromString(new math_1.Uint53(codeId).toString()),
+  //         label: label,
+  //         msg: (0, encoding_1.toUtf8)(JSON.stringify(msg)),
+  //         funds: [...(options.funds || [])],
+  //         admin: options.admin,
+  //     }),
+  // };
+
+  const instantiateContractMsg = {
+    typeUrl: '/cosmwasm.wasm.v1.MsgInstantiateContract',
+    value: tx_4.MsgInstantiateContract.fromPartial({
+      sender,
+      codeId: long.fromString(new math.Uint53(result.codeId).toString()),
+      label: 'burndrop',
+      msg: encoding.toUtf8(JSON.stringify(instantiateMsg)),
+      funds: [],
+      admin: sender,
+    }),
+  };
+
+  const accountFromSigner = (await signer.getAccounts()).find(
+    (account) => account.address === sender,
+  )!;
+  const pubkey = amino.encodeSecp256k1Pubkey(accountFromSigner.pubkey);
+  const anyMsgs = [instantiateContractMsg].map((m) =>
+    client.registry.encodeAsAny(m),
+  );
+  const { sequence } = await client.getSequence(sender);
+  // @ts-ignore
+  const queryClient = client.forceGetQueryClient();
+  const { gasInfo } = await queryClient.tx.simulate(
+    anyMsgs,
+    '',
+    pubkey,
+    sequence,
+  );
+  console.log(gasInfo);
+
+  const gasEstimation = math.Uint53.fromString(
+    gasInfo?.gasUsed.toString() || '0',
+  ).toNumber();
+  const multiplier = 2.3;
+  const usedFee = calculateFee(
+    Math.round(gasEstimation * multiplier),
+    GasPrice.fromString('0.02uluna'),
+  );
+
+  const res = await client.instantiate(
+    sender,
+    result.codeId,
+    instantiateMsg,
+    'burndrop',
+    usedFee,
+    { admin: sender },
+  );
+  console.log(res);
+}
+
+main().catch(console.error);
