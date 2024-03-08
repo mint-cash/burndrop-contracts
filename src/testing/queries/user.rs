734a19f8bbe57d6de02c11873565157134c7be37
Huiseong Yang <devonnuri@gmail.com>
2024-03-08 14:45:00 +0900

    Write test for querying compensation

--- src/testing/queries/user.rs
+++ src/testing/queries/user.rs
@@ -1,8 +1,8 @@
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Uint128};
 use cw_multi_test::Executor;
 
-use crate::msg::QueryMsg::UsersInfo;
-use crate::msg::{ExecuteMsg, UsersInfoResponse};
+use crate::msg::QueryMsg::{UserInfo, UsersInfo};
+use crate::msg::{ExecuteMsg, UserInfoResponse, UsersInfoResponse};
 use crate::testing::{instantiate, ADMIN, SECOND_REFERRER, USER};
 use crate::types::common::OrderBy;
 
@@ -77,4 +77,31 @@ fn test_query_users() {
     assert_eq!(users.len(), 2);
     assert_eq!(users[0].0, "referrer2");
     assert_eq!(users[1].0, "user1");
+}
+
+#[test]
+fn test_query_compensation() {
+    let (mut app, burn_contract) = instantiate::default();
+
+    let address = "terra1rw43xy5388meu2pl3v02ckt8c754r73yhhv6qq".to_string();
+
+    let msg = ExecuteMsg::RegisterStartingUser {
+        user: address.clone(),
+    };
+    app.execute_contract(Addr::unchecked(ADMIN), burn_contract.addr(), &msg, &[])
+        .unwrap();
+
+
+    let query_res: UserInfoResponse = app
+        .wrap()
+        .query_wasm_smart(
+            burn_contract.addr(),
+            &UserInfo {
+                address: address.clone(),
+            },
+        )
+        .unwrap();
+
+    assert_eq!(query_res.compensation.oppamint, Uint128::new(365093610));
+    assert_eq!(query_res.compensation.ancs, Uint128::new(365093610));
 }
